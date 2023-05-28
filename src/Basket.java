c665130721d897fbb893a70bbe35593e47b44962
Pavel Khudenko <khudenk0pave1@gmail.com>
2023-05-29 00:33:42 +0300

    git commit -m 'new commit'

--- src/Basket.java
+++ src/Basket.java
@@ -1,35 +1,42 @@
 import java.io.*;
-import java.util.Scanner;
+import java.util.*;
 
-public class Basket implements Serializable {
-    private String[] products;
-    private int[] quantities;
-    private int[] prices;
+class Basket implements Serializable {
+    private List<String> products;
+    private List<Integer> quantities;
+    private List<Integer> prices;
 
     public Basket() {
-        products = new String[5];
-        quantities = new int[5];
-        prices = new int[5];
+        products = new ArrayList<>();
+        quantities = new ArrayList<>();
+        prices = new ArrayList<>();
     }
 
-    public void addToCart(int productNum, int quantity) {
-        quantities[productNum] += quantity;
+    public void addToCart(String product, int quantity, int price) {
+        products.add(product);
+        quantities.add(quantity);
+        prices.add(price);
     }
 
     public void printCart() {
         System.out.println("Содержимое корзины:");
-        for (int i = 0; i < products.length; i++) {
-            if (quantities[i] > 0) {
-                System.out.println(products[i] + ": " + quantities[i] + " шт.");
-            }
+        for (int i = 0; i < products.size(); i++) {
+            String product = products.get(i);
+            int quantity = quantities.get(i);
+            int price = prices.get(i);
+            System.out.println(product + ": " + quantity + " шт. по " + price + " руб.");
         }
     }
 
     public void saveTxt(File textFile) {
         try (PrintWriter writer = new PrintWriter(textFile)) {
-            for (int i = 0; i < products.length; i++) {
-                writer.println(products[i] + ":" + quantities[i] + ":" + prices[i]);
+            for (int i = 0; i < products.size(); i++) {
+                String product = products.get(i);
+                int quantity = quantities.get(i);
+                int price = prices.get(i);
+                writer.println(product + ":" + quantity + ":" + price);
             }
+            System.out.println("Корзина успешно сохранена в файл " + textFile.getName());
         } catch (FileNotFoundException e) {
             System.out.println("Ошибка сохранения корзины в файл.");
             e.printStackTrace();
@@ -38,60 +45,26 @@ public class Basket implements Serializable {
 
     public static Basket loadFromTxtFile(File textFile) {
         try (Scanner scanner = new Scanner(textFile)) {
-            String[] products = new String[5];
-            int[] quantities = new int[5];
-            int[] prices = new int[5];
+            Basket basket = new Basket();
 
-            int index = 0;
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 String[] parts = line.split(":");
-                products[index] = parts[0];
-                quantities[index] = Integer.parseInt(parts[1]);
-                prices[index] = Integer.parseInt(parts[2]);
-                index++;
+
+                if (parts.length >= 3) {
+                    String product = parts[0];
+                    int quantity = Integer.parseInt(parts[1]);
+                    int price = Integer.parseInt(parts[2]);
+
+                    basket.addToCart(product, quantity, price);
+                }
             }
 
-            Basket basket = new Basket();
-            basket.setProducts(products);
-            basket.setQuantities(quantities);
-            basket.setPrices(prices);
+            System.out.println("Корзина успешно загружена из файла " + textFile.getName());
             return basket;
         } catch (FileNotFoundException e) {
             System.out.println("Файл корзины не найден. Создана новая корзина.");
-            return null;
         }
-    }
-
-    public void saveBin(File file) {
-        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(file))) {
-            outputStream.writeObject(this);
-        } catch (IOException e) {
-            System.out.println("Ошибка сохранения корзины в бинарный файл.");
-            e.printStackTrace();
-        }
-    }
-
-    public static Basket loadFromBinFile(File file) {
-        try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(file))) {
-            return (Basket) inputStream.readObject();
-        } catch (IOException | ClassNotFoundException e) {
-            System.out.println("Файл корзины не найден. Создана новая корзина.");
-            return null;
-        }
-    }
-
-    // Геттеры и сеттеры для полей products, quantities, prices
-
-    public void setProducts(String[] products) {
-        this.products = products;
-    }
-
-    public void setQuantities(int[] quantities) {
-        this.quantities = quantities;
-    }
-
-    public void setPrices(int[] prices) {
-        this.prices = prices;
+        return new Basket();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,55 @@
-import java.io.*;
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Basket basket = Basket.loadFromBinFile(new File("basket.bin"));
-
-        if (basket == null) {
-            basket = new Basket();
-        }
+        File basketFile = new File("basket.txt");
+        Basket basket = Basket.loadFromTxtFile(basketFile);
 
         Scanner scanner = new Scanner(System.in);
-        int option = -1;
+        int choice;
 
-        while (option != 0) {
+        while (true) {
             System.out.println("Меню:");
             System.out.println("1. Добавить товар в корзину");
             System.out.println("2. Вывести содержимое корзины");
-            System.out.println("0. Сохранить корзину и выйти");
-            System.out.print("Выберите опцию: ");
-            option = scanner.nextInt();
-            scanner.nextLine(); // Очистка буфера
+            System.out.println("3. Сохранить корзину в файл");
+            System.out.println("0. Выход");
 
-            if (option == 1) {
-                System.out.print("Введите номер товара и количество (через пробел): ");
-                String inputString = scanner.nextLine();
-                String[] parts = inputString.split(" ");
-                int productNum = Integer.parseInt(parts[0]) - 1;
-                int quantity = Integer.parseInt(parts[1]);
-                basket.addToCart(productNum, quantity);
-            } else if (option == 2) {
-                basket.printCart();
-            } else if (option == 0) {
-                basket.saveBin(new File("basket.bin"));
-                System.out.println("Корзина сохранена в файл basket.bin");
-            } else {
-                System.out.println("Неверная опция. Повторите ввод.");
+            System.out.print("Выберите действие: ");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Введите название товара: ");
+                    String product = scanner.nextLine();
+
+                    System.out.print("Введите количество: ");
+                    int quantity = scanner.nextInt();
+
+                    System.out.print("Введите цену: ");
+                    int price = scanner.nextInt();
+                    scanner.nextLine();
+
+                    basket.addToCart(product, quantity, price);
+                    System.out.println("Товар успешно добавлен в корзину.");
+                    break;
+                case 2:
+                    basket.printCart();
+                    break;
+                case 3:
+                    basket.saveTxt(basketFile);
+                    break;
+                case 0:
+                    basket.saveTxt(basketFile);
+                    System.out.println("Корзина сохранена. Программа завершена.");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Неверный выбор. Попробуйте снова.");
+                    break;
             }
         }
     }
 }
-
